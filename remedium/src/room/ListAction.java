f84a0d0435cdc7523a8d3624b41a7b787c8614f9
이동훈 <hun017017@gmail.com>
2017-04-14 13:18:20 +0900

    room 액션 수정3

--- remedium/src/room/ListAction.java
+++ remedium/src/room/ListAction.java
@@ -1,8 +1,23 @@
 package room;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class ListAction extends ActionSupport {
+	
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	public ListAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
 
 	@Override
 	//검색된 객실 리스트
