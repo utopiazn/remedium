30ceefd0f7f07d007f4a0ac364eee92fa9e55a92
이동훈 <hun017017@gmail.com>
2017-04-14 13:40:23 +0900

    adminmember 관리 액션추가

--- remedium/src/admin/member/AdminListAction.java
+++ remedium/src/admin/member/AdminListAction.java
@@ -1,5 +1,34 @@
 package admin.member;
 
-public class AdminListAction {
+import java.io.IOException;
+import java.io.Reader;
 
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AdminListAction extends ActionSupport{
+	
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	public AdminListAction() throws IOException {
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	
+	@Override
+	//가입된 고객들의 리스트
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+	//관리자 메인페이지
+	public String main() throws Exception {
+		return SUCCESS;
+	}
+
+	
 }
