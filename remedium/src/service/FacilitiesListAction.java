170a8360c900d743f87cfc3514365ef2d7359b90
장조성 <utopiazn@naver.com>
2017-04-14 12:59:36 +0900

    편의시설들의 목록 action 추가

--- remedium/src/service/FacilitiesListAction.java
+++ remedium/src/service/FacilitiesListAction.java
@@ -1,5 +1,40 @@
 package service;
 
-public class FacilitiesListAction {
 
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+public class FacilitiesListAction extends ActionSupport{
+	
+	public static Reader reader; //파일 스트림을 위한 reader
+	public static SqlMapClient sqlMapper; //SqlMapClient API를 사용하기 위한 sqlMapper 객체	
+	
+	//생성자
+	public FacilitiesListAction() throws IOException{
+		
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml"); //sqlMapConfig.xml 파일의 설정 내용을 가져온다
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);  //sqlMapConfig.xml 내용을 적용한 sqlMapper 객체 생성
+		
+		reader.close();
+	}
+
+	
+	//편의시설들의 목록
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
+
+	
+	
+	
+	
+	
 }
