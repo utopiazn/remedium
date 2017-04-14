bc41b8d7a6658ad427d08cdae1a3449aca6b85b6
안예아 <anyeah6>
2017-04-14 13:39:34 +0900

    FAQ 수정이랑 편의시설

--- remedium/src/admin/FaqAction.java
+++ remedium/src/admin/FaqAction.java
@@ -20,26 +20,31 @@ public class FaqAction extends ActionSupport {
 		reader.close();
 	}
 	
+	//관리자 글쓰기 폼
 	public String insForm() throws Exception{
 		
 		return SUCCESS;
 	}
 	
+	//관리자 글쓰기	
 	public String execute() throws Exception{
 		
 		return SUCCESS;
 	}
 	
+	//관리자 수정폼	
 	public String updForm() throws Exception{
 		
 		return SUCCESS;
 	}
 	
+	//관리자 글 수정 처리	
 	public String update() throws Exception{
 		
 		return SUCCESS;
 	}
 	
+	//관리자 글 삭제
 	public String delete() throws Exception{
 		
 		return SUCCESS;
