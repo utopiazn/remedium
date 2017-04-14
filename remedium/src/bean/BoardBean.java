78635f295118f1cce125340645803499ee7fd971
안예아 <anyeah6>
2017-04-14 12:21:54 +0900

    boardbean 완료

--- remedium/src/bean/BoardBean.java
+++ remedium/src/bean/BoardBean.java
@@ -1,6 +1,76 @@
 package bean;
 
+import java.sql.Date;
+
 public class BoardBean {
 
+	private int no;
+	private String subject;
+	private String memberID;
+	private String name;
+	private String content;
+	private String ref;
+	private String re_step;
+	private Date regdate;
+	private String type;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMemberID() {
+		return memberID;
+	}
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRef() {
+		return ref;
+	}
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	public String getRe_step() {
+		return re_step;
+	}
+	public void setRe_step(String re_step) {
+		this.re_step = re_step;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+	
 
 }
