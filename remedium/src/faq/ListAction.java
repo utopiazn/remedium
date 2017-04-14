130e630d3b32efd46b5a31916d3718d1257f1bfe
허어녕 <hueonyung>
2017-04-14 14:51:24 +0900

    d

--- remedium/src/faq/ListAction.java
+++ remedium/src/faq/ListAction.java
@@ -8,23 +8,22 @@ import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ListAction extends ActionSupport{
-	
+public class ListAction extends ActionSupport {
+
 	public static Reader reader;
 	public static SqlMapClient sqlMapper;
 
-	
-public ListAction() throws IOException {
-		
+	public ListAction() throws IOException {
+
 		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
-		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);	
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
 		reader.close();
 	}
 
-//자주묻는 질문 리스트
-public String excute() throws Exception{
-	
-	return SUCCESS;
-}
+	// 자주묻는 질문 리스트
+	public String excute() throws Exception {
+
+		return SUCCESS;
+	}
 
 }
