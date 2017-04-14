6c91394e8f928be3e417328eb9682e93c7c010cd
안예아 <anyeah6>
2017-04-14 13:47:37 +0900

    메인이랑 인포

--- remedium/src/main/MainAction.java
+++ remedium/src/main/MainAction.java
@@ -1,5 +1,29 @@
 package main;
 
-public class MainAction {
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MainAction extends ActionSupport {
+	
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+
+	public MainAction() throws IOException{
+		
+		reader = Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper = SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	
+	//사이트 메인 페이지
+	public String main() throws Exception{
+		
+		return SUCCESS;
+	}
 
 }
