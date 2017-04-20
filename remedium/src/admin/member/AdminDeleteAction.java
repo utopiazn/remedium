eea27d6c5859f90556eaa7208079c6953e265561
허어녕 <hueonyung>
2017-04-20 11:10:06 +0900

    삭제 xml  추가

--- remedium/src/admin/member/AdminDeleteAction.java
+++ remedium/src/admin/member/AdminDeleteAction.java
@@ -8,10 +8,17 @@ import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.opensymphony.xwork2.ActionSupport;
 
+import bean.MemberBean;
+
 public class AdminDeleteAction extends ActionSupport{
 	
 	public static Reader reader;
 	public static SqlMapClient sqlMapper;
+	
+	private String memberID;
+	
+	MemberBean paramClass;
+	MemberBean resultClass;
 
 	public AdminDeleteAction() throws IOException{
 		
@@ -22,10 +29,36 @@ public class AdminDeleteAction extends ActionSupport{
 	//회원 탈퇴
 	public String execute() throws Exception{
 		
+		paramClass = new MemberBean();
+		resultClass = new MemberBean();
+		
+		paramClass.setMemberID(getMemberID());
+		
+		sqlMapper.delete("member.deleteMember", paramClass);
+				
 		return SUCCESS;
 	}
 	
+	public String getMemberID() {
+		return memberID;
+	}
+	public MemberBean getParamClass() {
+		return paramClass;
+	}
+	public MemberBean getResultClass() {
+		return resultClass;
+	}
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
+	public void setParamClass(MemberBean paramClass) {
+		this.paramClass = paramClass;
+	}
+	public void setResultClass(MemberBean resultClass) {
+		this.resultClass = resultClass;
+	}
 	
-
+	
+	
 }
 
