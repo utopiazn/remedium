594258429ece9d466e966bad1e37c89ecbd2f529
허어녕 <hueonyung>
2017-04-20 14:55:54 +0900

    ㅇㅇ

--- remedium/src/board/WriteAction.java
+++ remedium/src/board/WriteAction.java
@@ -3,6 +3,8 @@ package board;
 import java.io.IOException;
 import java.io.Reader;
 
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
@@ -20,12 +22,14 @@ public class WriteAction extends ActionSupport {
 	}
 	
 	// 문의 사항 게시글 쓰기 폼
-	public String form() throws Exception {
+	public String form() throws Exception {	
+		
 		return SUCCESS;
 	}
 
 	// 문의 사항 게시글 쓰기 처리
 	public String execute() throws Exception {
 		return SUCCESS;
+		
 	}
 }
