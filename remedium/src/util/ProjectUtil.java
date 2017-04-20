ddd5a029d67acea091828836a0953a82ee7ca1c6
허어녕 <hueonyung>
2017-04-20 11:00:56 +0900

    날짜 수정

--- remedium/src/util/ProjectUtil.java
+++ remedium/src/util/ProjectUtil.java
@@ -5,18 +5,23 @@ import java.util.Date;
 
 public class ProjectUtil {
 	
+	// 현재 날짜 
 	public String currentDate(){
 		Date currentDate = new Date();
 		SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd" );
 		String cDate = dateFormat.format(currentDate);
-		System.out.println(cDate);
+	
+		System.out.println("현재날짜 : "+cDate);
 		
 		return cDate;
 	}
 	
+	// 프로젝트 위치 경로
 	public String getPath(){
 		String s = this.getClass().getResource("/").getPath();
 		String sc = s.substring(0, s.indexOf(".metadata"));
+		
+		System.out.println("프로젝트 전까지의 경로: "+sc);
 		
 		return sc;
 	} 
