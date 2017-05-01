54cc3d1475fa4d1a3527c6cb0065befd7beee304
장조성 <utopiazn@naver.com>
2017-05-01 13:50:51 +0900

    메인 화면에 객실소개 이름 리스트 추가 작업

--- remedium/src/main/MainAction.java
+++ remedium/src/main/MainAction.java
@@ -12,31 +12,41 @@ import bean.RoomclassBean;
 import util.ProjectUtil;
 
 
-public class MainAction extends ActionSupport implements SessionAware{
+
+public class MainAction extends ActionSupport implements SessionAware {
 	
+
 	private List<RoomclassBean> list;
+	
+	
 	
 	private Map session;
 	
 	//사이트 메인 페이지
 	public String main() throws Exception{
 		
-		list = new ArrayList<RoomclassBean>();
-		
-		list = util.ProjectUtil.sqlMapper.queryForList("roomclassSQL.selectAll");
-		System.out.println(list.size());
-		session.put("RClist", list);
-		
+		//객실소개 이름 리스트 가져오기
+		ProjectUtil rcs = new ProjectUtil(session);		
+		rcs.roomClassSession();
 		
 		return SUCCESS;
 	}
+	
+	public void setSession(Map session) {
+		this.session = session;
+	}
 
 	public Map getSession() {
+		return session;
+	}
+	
+	/*public Map getSession() {
 		return session;
 	}
 
 	public void setSession(Map session) {
 		this.session = session;
 	}
-
+*/
+	
 }
